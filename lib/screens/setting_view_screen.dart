a0147e00107a1f965c72b44b00f0d24bef530f1a
Renan Johny Ortega <renanortega82@gmail.com>
2023-01-18 12:35:08 -0300

    feat: add page view.

--- lib/screens/setting_view_screen.dart
+++ lib/screens/setting_view_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gasteei/screens/expanses_screen.dart';
-import 'package:gasteei/screens/home_screen.dart';
+import 'package:gasteei/screens/expenses_screen.dart';
+import 'package:gasteei/screens/home/home_screen.dart';
 import 'package:gasteei/screens/profile_screen.dart';
 import 'package:gasteei/widgets/bottom_bar.dart';
 
@@ -12,9 +12,34 @@ class SettingViewScreen extends StatefulWidget {
 }
 
 class _SettingViewScreenState extends State<SettingViewScreen> {
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void navigatioTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
   int _selectedIndex = 0;
 
-  static const List<Widget> _widgetOptions = <Widget>[HomeScreen(), ExpansesScreen(), Text("BarChartScreen"), ProfileScreen()];
+  static const List<Widget> _widgetOptions = <Widget>[HomeScreen(), ExpensesScreen(), Text("BarChartScreen"), ProfileScreen()];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -26,10 +51,15 @@ class _SettingViewScreenState extends State<SettingViewScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      body: _widgetOptions[_selectedIndex],
+      body: PageView(
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        children: _widgetOptions,
+      ),
+      //body: _widgetOptions[_selectedIndex],
       bottomNavigationBar: BottomNavigationBarBlur(
-        currentIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
+        currentIndex: _page,
+        onItemTapped: navigatioTapped,
       ),
     );
   }
